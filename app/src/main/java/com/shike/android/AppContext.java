f6a2748847eb653021d5da28d9ebe22a604e141e
ftdzyb <ftdzyb@163.com>
2017-11-09 11:24:22 +0800

    Initial commit

---
+++ app/src/main/java/com/shike/android/AppContext.java
@@ -0,0 +1,18 @@
+package com.shike.android;
+
+import android.app.Application;
+
+/**
+ * @文件名: AppContext
+ * @功能描述:
+ * @Create by zengyb on 2017/11/9
+ * @修改记录:
+ */
+public class AppContext extends Application{
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
